fd1d0831683589346b7afc559362d3bd9970095c
alannguyen00 <58610714+alannguyen00@users.noreply.github.com>
2020-02-06 13:52:28 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,40 @@
+function bad_guy () {
+    bad = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . b b . . . . 
+. . . . b . . . . b d d . . . . 
+. . . b d b . b b d d d d b . . 
+. . . b d d b d d d d d d b . . 
+. . . b d d d d d d d d d d b . 
+. . b d d d d d d d d d d d b . 
+. b d d d d d d d d d d d d b . 
+. . b d d d d d d d d d d d b . 
+. . b d d d d d d d d d d d b . 
+. . . b b d d d d d d d d b . . 
+. . . . . b d d d d d d b . . . 
+. . . . . . b d d d d b . . . . 
+. . . . . . . b b b b . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, SpriteKind.Enemy)
+    bad.setPosition(scene.screenWidth(), Math.randomRange(0, scene.screenHeight()))
+    extra_velocity = 0
+    if (Math.percentChance(20)) {
+        extra_velocity = Math.randomRange(0, 50)
+    } else {
+        extra_velocity = Math.randomRange(-16, 16)
+    }
+    bad.vx = (-50 - 5) * extra_velocity
+    if (info.score() <= 20) {
+        controller.moveSprite(Space_Coupe, 100 - 2, 100)
+    }
+}
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
+    game_over()
+})
+function game_over () {
+    game.over(false)
+}
 function shippy () {
     Space_Coupe = sprites.create(img`
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
@@ -66,49 +103,12 @@ function shippy () {
     Space_Coupe.setFlag(SpriteFlag.StayInScreen, true)
     info.setScore(5)
 }
-function bad_guy () {
-    bad = sprites.create(img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . b b . . . . 
-. . . . b . . . . b d d . . . . 
-. . . b d b . b b d d d d b . . 
-. . . b d d b d d d d d d b . . 
-. . . b d d d d d d d d d d b . 
-. . b d d d d d d d d d d d b . 
-. b d d d d d d d d d d d d b . 
-. . b d d d d d d d d d d d b . 
-. . b d d d d d d d d d d d b . 
-. . . b b d d d d d d d d b . . 
-. . . . . b d d d d d d b . . . 
-. . . . . . b d d d d b . . . . 
-. . . . . . . b b b b . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-`, SpriteKind.Enemy)
-    bad.setPosition(scene.screenWidth(), Math.randomRange(0, scene.screenHeight()))
-    extra_velocity = 0
-    if (Math.percentChance(20)) {
-        extra_velocity = Math.randomRange(0, 50)
-    } else {
-        extra_velocity = Math.randomRange(-16, 16)
-    }
-    bad.vx = (-50 - 5) * extra_velocity
-    if (info.score() <= 20) {
-        controller.moveSprite(Space_Coupe, 100 - 2, 100)
-    }
-}
-sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
-    game_over()
-})
-function game_over () {
-    game.over(false)
-}
 function change_score () {
 	
 }
+let Space_Coupe: Sprite = null
 let extra_velocity = 0
 let bad: Sprite = null
-let Space_Coupe: Sprite = null
 shippy()
 // For scoreboard
 game.onUpdateInterval(2000, function () {
